d5701c0b6afc6af59d3b32927211bd9c2f31bfc2
Kristina <kristimayor@gmail.com>
2026-06-22 00:15:20 +0300

    fix reviews page module export

---
+++ app/reviews/page.tsx
@@ -0,0 +1,7 @@
+export default function ReviewsPage() {
+  return (
+    <div className="min-h-screen bg-black text-white p-10">
+      <h1 className="text-5xl font-bold">Отзывы</h1>
+    </div>
+  );
+}
