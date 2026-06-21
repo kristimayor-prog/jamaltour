1be671ac560099f6224dfa93ccf9ff54c2e801a4
Kristina <kristimayor@gmail.com>
2026-06-22 00:05:34 +0300

    fix egypt page build error

---
+++ app/egypt/page.tsx
@@ -0,0 +1,11 @@
+export default function EgyptPage() {
+  return (
+    <div className="min-h-screen bg-black text-white p-10">
+      <h1 className="text-5xl font-bold mb-6">Египет</h1>
+
+      <p className="text-xl text-gray-400">
+        Лучшие экскурсии в Аланье, Хургаде и Грузии
+      </p>
+    </div>
+  );
+}
